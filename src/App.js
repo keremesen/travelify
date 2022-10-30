68a364b35bc95734a0eff833bbfc268b1d887933
Kerem Esen <kerem.esen.01@hotmail.com>
2022-10-31 00:50:41 +0300

    map added

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <Map />
+          <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <List />
+          <Map />
         </Grid>
       </Grid>
     </>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { LocationOnOutlined } from "@material-ui/icons";
+import { Rating } from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Map = () => {
-  return (
-    <div>Map</div>
-  )
-}
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px");
 
-export default Map
+  const coordinates = { lat:0, lng:0}
+
+  return <div className={classes.mapContainer}>
+    <GoogleMapReact
+    bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_MAP_API_KEY}}
+    defaultCenter={coordinates}
+    center={coordinates}
+    defaultZoom={14}
+    margin={[50,50,50,50]}
+    
+    >
+
+    </GoogleMapReact>
+
+
+  </div>;
+};
+
+export default Map;
