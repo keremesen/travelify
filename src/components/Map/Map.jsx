49920398ff0aab4fbd36101d0b8fe0a8bc7dcabb
Kerem Esen <kerem.esen.01@hotmail.com>
2022-11-19 23:22:27 +0300

    api configuration
    travel advisor

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,26 +6,25 @@ import { Rating } from "@material-ui/lab";
 
 import useStyles from "./styles";
 
-const Map = () => {
+const Map = ({setCoordinates, setBounds, coordinates}) => {
   const classes = useStyles();
   const isMobile = useMediaQuery("(min-width:600px");
 
-  const coordinates = { lat:0, lng:0}
-
-  return <div className={classes.mapContainer}>
-    <GoogleMapReact
-    bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_MAP_API_KEY}}
-    defaultCenter={coordinates}
-    center={coordinates}
-    defaultZoom={14}
-    margin={[50,50,50,50]}
-    
-    >
-
-    </GoogleMapReact>
-
-
-  </div>;
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={7}
+        margin={[50, 50, 50, 50]}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
