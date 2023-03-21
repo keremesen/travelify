5b725c1ee7683a3b257896b551ea61e476cd8241
Kerem Esen <kerem.esen.01@hotmail.com>
2023-03-22 02:54:40 +0300

    scroll places

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ const App = () => {
   const [places, setPlaces] = useState([])
   const [coordinates, setCoordinates] = useState()
   const [bounds, setBounds] = useState("")
+  const [childClicked, setChildClicked] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  
+
 
   useEffect(()=>{
     navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude } })=>{
@@ -21,11 +25,12 @@ const App = () => {
   },[])
 
   useEffect(() => {
+    setIsLoading(true)
     getPlacesData(bounds.sw, bounds.ne)
     
     .then((data)=>{
       setPlaces(data)
-      console.log(data)
+      setIsLoading(false)
     })
   }, [coordinates, bounds]);
 
@@ -35,7 +40,9 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List places={places} />
+          <List places={places}
+          childClicked={childClicked}
+          isLoading={isLoading} />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
@@ -43,6 +50,7 @@ const App = () => {
           setBounds = {setBounds}
           coordinates = {coordinates}
           places={places}
+          setChildClicked={setChildClicked}
           />
         </Grid>
       </Grid>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import { LocationOnOutlined } from "@material-ui/icons";
@@ -6,7 +6,7 @@ import { Rating } from "@material-ui/lab";
 
 import useStyles from "./styles";
 
-const Map = ({setCoordinates, setBounds, coordinates, places}) => {
+const Map = ({setCoordinates, setBounds, coordinates, places, setChildClicked}) => {
   const classes = useStyles();
   const isDesktop = useMediaQuery("(min-width:600px");
 
@@ -22,6 +22,7 @@ const Map = ({setCoordinates, setBounds, coordinates, places}) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
+        onChildClick={(child)=> {setChildClicked(child)}}
       >
         {places?.map((place , i)=>(
           <div className={classes.markerContainer}
